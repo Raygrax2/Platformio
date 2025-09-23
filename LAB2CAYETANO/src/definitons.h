064247cc59f0b56d212c69edc99f9743ede03070
Santiago <SantiMF20@proton.me>
2025-09-23 17:21:28 -0600

    Add all the needed libraries, definitions and main code

---
+++ LAB2CAYETANO/src/definitons.h
@@ -0,0 +1,14 @@
+#ifndef __DEFINITONS_H__
+#define __DEFINITONS_H__
+
+#include <stdio.h>
+#include <string.h>
+#include "SimpleRGB.h"
+#include "SimpleTimer.h"
+#include "SimpleGPIO.h"
+#include "SimpleADC.h"
+#include "SimpleUART.h"
+#include "SimpleI2C.h"
+
+
+#endif // __DEFINITIONS_H__
---
+++ LAB2CAYETANO/src/main.cpp
@@ -0,0 +1,6 @@
+#include "definitons.h" 
+
+extern "C" void app_main()
+{
+   
+}
