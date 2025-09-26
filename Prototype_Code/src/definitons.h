d73f0824614b18f390bea4320e4bc98ce7bf8fb3
Santiago <SantiMF20@proton.me>
2025-09-26 14:33:51 -0600

    Declaration fo new project, already base files and libraries, the code is almost raw

---
+++ Prototype_Code/src/definitons.h
@@ -0,0 +1,22 @@
+#ifndef __DEFINITONS_H__
+#define __DEFINITONS_H__
+
+#include <stdio.h>
+#include <string.h>
+#include "SimpleRGB.h"
+#include "SimpleTimer.h"
+#include "SimpleGPIO.h"
+#include "SimpleADC.h"
+#include "SimpleUART.h"
+#include "SimplePWM.h"
+#include "Filter.h"
+//Pins 
+
+//Timers 
+SimpleTimer timer;
+
+
+
+
+
+#endif // __DEFINITONS_H__
---
+++ Prototype_Code/src/main.cpp
@@ -0,0 +1,23 @@
+#include "definitons.h"
+
+static void IRAM_ATTR timerInterrupt(){
+    timer.setInterrupt();
+}
+
+//añadir interrups
+extern "C" void app_main()
+{
+    esp_task_wdt_deinit();
+    float filteredValue = 0.0;
+    timer.setup(timerInterrupt, "Timer1");
+    timer.startPeriodic(1000);
+    while(1)
+    {
+        
+        if (timer.interruptAvailable())
+        {
+           
+        }
+        
+    }
+}
