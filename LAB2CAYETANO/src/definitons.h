9fb1487a501d035287023e4135756052251a2a8b
Santiago <SantiMF20@proton.me>
2025-09-29 20:37:28 -0600

    latest changes, adds the uart and some coeficients of the filter

--- LAB2CAYETANO/src/definitons.h
+++ LAB2CAYETANO/src/definitons.h
@@ -10,10 +10,18 @@
 #include "SimpleUART.h"
 #include "SimplePWM.h"
 #include "Filter.h"
+//Timer for PWM
+TimerConfig PWM_Time{
+    .timer = LEDC_TIMER_3,
+    .frequency = 100,
+    .bit_resolution = LEDC_TIMER_14_BIT,
+    .mode = LEDC_LOW_SPEED_MODE};
 //Pins should be changed based on disponibilty
+
 SimplePWM Square;
 SimpleADC ADC;
 SimpleUART UART_RAW(115200);
+SimpleUART UART_Filtered(115200);
 
 
 uint8_t ADC_pin = 24; 
@@ -23,6 +31,21 @@ SimpleTimer timer;
 
 
 
+Filter LPF;
+Filter HPF;
+Filter MA;
+
+
+//Filter Coefficients
+//Low Pass Filter
+float a_LPF[3] = {1, -1.3073, 0.4918};
+float b_LPF[3] = {0.0461, 0.0923, 0.0461};
+//High Pass Filter
+float b_HPF[3] = {0.978030726982, -1.956061453964, 0.978030726982};
+float a_HPF[3] = {1.0, -1.955578240315, 0.956543676511};
+//Moving Average Filter
+float b_MA[3] = {0.3333, 0.3333, 0.3333};
+float a_MA[3] = {1.0, 0.0, 0.0};
 
 
 #endif // __DEFINITONS_H__
--- LAB2CAYETANO/src/main.cpp
+++ LAB2CAYETANO/src/main.cpp
@@ -1,8 +1,5 @@
 #include "definitons.h"
 
-static void IRAM_ATTR timerInterrupt(){
-    timer.setInterrupt();
-}
 
 //añadir interrups
 extern "C" void app_main()
@@ -10,19 +7,22 @@ extern "C" void app_main()
     esp_task_wdt_deinit();
     float filteredValue = 0.0;
     ADC.setup(ADC_pin);
-    timer.setup(timerInterrupt, "Timer1");
-    timer.startPeriodic(1000);
-    // Square.setup(pinSquare, channelSquare, timer);
-    
+    LPF.setup(b_LPF, a_LPF,3,3);
+    ADC.setup(ADC_pin);
+    Square.setup(pinSquare, channelSquare, &PWM_Time, false);
     //HPF.setup(b_HPF, a_HPF);
     //MA.setup(b_MA, a_MA);
     while(1)
     {
-        
-        if (timer.interruptAvailable())
-        {
-           
+        filteredValue = LPF.apply(ADC.read(ADC_READ_RAW));
+        //filteredValue = HPF.apply((float)ADC.read());
+        //filteredValue = MA.apply((float)ADC.read());
+        if(UART_RAW.available()){
+            UART_RAW.write(ADC.read(ADC_READ_RAW), sizeof(ADC.read(ADC_READ_RAW));
         }
-        
+        if(UART_Filtered.available()){
+            UART_Filtered.write((char*)&filteredValue, sizeof(filteredValue));
+        }
+
     }
 }
