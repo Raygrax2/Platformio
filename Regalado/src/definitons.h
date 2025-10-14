3e90278a42c989aa0b8cab51613897b5468505ef
Santiago <SantiMF20@proton.me>
2025-10-14 10:15:27 -0600

    Regalado

--- Regalado/src/definitons.h
+++ Regalado/src/definitons.h
@@ -22,8 +22,20 @@
 #include "SimplePWM.h"
 #include "Filter.h"
 
+TimerConfig PWM_TimerA{
+    .timer = LEDC_TIMER_0,
+    .frequency = 20000,                 // Frecuencia PWM motor A (Hz)
+    .bit_resolution = LEDC_TIMER_10_BIT,
+    .mode = LEDC_LOW_SPEED_MODE
+};
+BDCMotor motor;
+uint8_t motor_pins[2] = {18, 19}; // Pines de control del motor
+uint8_t motor_ch[2] = {0, 1};      // Canales PWM del motor
+
 SimpleADC adc;
 uint8_t adc_pin = 34; // Pin de lectura ADC
 SimpleTimer timer;
+float lower_treshold = 2000.0; // Umbral inferior en mV
+float upper_treshold = 4000.0; // Umbral superior en mV
 
 #endif // __DEFINITIONS_H__
